8f11a5d08e58b1b21baf7ac44f650fc5b8efd887
devikumavath <devikumavath1999@gmail.com>
2023-06-07 08:20:15 +0530

    added 24th project

---
+++ 24tellMeAJoke/script.js
@@ -0,0 +1,22 @@
+const get = document.getElementById('get'); 
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click' , async () => {
+    const getJoke =  await fetch('https://icanhazdadjoke.com/' , {
+
+    headers : {
+        accept : 'application/json'
+    }
+
+    })
+
+    const  data = await getJoke.json()
+    const joke = data.joke
+
+    get.innerText = joke;
+
+
+
+    console.log(joke);
+})
